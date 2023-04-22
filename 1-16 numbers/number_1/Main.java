436f3ecd217df54b203e51cf2c4dbc63606e6032
Krivcova-VA <112896058+Krivcova-VA@users.noreply.github.com>
2023-04-22 12:36:15 +0300

    hw_1

---
+++ 1-16 numbers/number_1/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        Mtwo a = new Mtwo(3);
+        System.out.println(a.transposeMatrix(a));
+        System.out.println(a);
+    }
+}
