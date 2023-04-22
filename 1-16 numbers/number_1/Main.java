14b918c665e35a357a78e301517bb15462b689ea
Krivcova-VA <112896058+Krivcova-VA@users.noreply.github.com>
2023-04-22 12:54:49 +0300

    Delete 1-16 numbers/number_1 directory

--- 1-16 numbers/number_1/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-public class Main {
-    public static void main(String[] args) {
-        Mtwo a = new Mtwo(3);
-        System.out.println(a.transposeMatrix(a));
-        System.out.println(a);
-    }
-}
