bf5f220e40dea9c68c31efd653b9dfd2e436b621
Krivcova-VA <112896058+Krivcova-VA@users.noreply.github.com>
2023-04-22 12:55:26 +0300

    Delete 1-16 numbers/number_4 directory

--- 1-16 numbers/number_4/Count.java
+++
@@ -1,9 +0,0 @@
-public class Count {
-    public int str_count (String str, char ch) {
-        int cnt = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ch) cnt += 1;
-        }
-        return cnt;
-    }
-}
--- 1-16 numbers/number_4/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String str = "Hello";
-        char ch = 'l';
-        Count instance = new Count();
-        System.out.println(instance.str_count(str, ch));
-    }
-}
